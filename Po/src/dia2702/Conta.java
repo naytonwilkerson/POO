977f3d8c5236eafe725d605f1542ca344e8220a6
Nayton Wilkerson <Nayton Wilkerson@DESKTOP-78A6OJQ>
2018-02-27 21:07:29 -0300

    Exercícios 27-02-2018

---
+++ Po/src/dia2702/Conta.java
@@ -0,0 +1,32 @@
+package dia2702;
+
+public class Conta {
+
+	private String titular;
+
+	public Conta() {
+		
+		//* Para que Seja possivel ser executada uma ação sem que o úsuario interaja com o software. //*
+			
+		
+		
+		System.out.println(" ");
+		
+	}
+
+	public Conta(String titular) {
+		super();
+		this.titular = titular;
+		
+		System.out.println(this.titular);
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+}
---
+++ Po/src/dia2702/Main.java
@@ -0,0 +1,19 @@
+package dia2702;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		Conta conta = new Conta("Nayton");
+		
+		// * É necessario dois contrutores para que passar ou não argumentos seja opcional//*
+
+		Conta c = new Conta();
+
+		// * O objeto conta não está publico.//*
+
+	}
+
+}
